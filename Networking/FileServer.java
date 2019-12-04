040a8bd36175c801b797efab335cfd9e8a4ab6af
Faiz Ahmed <31391440+faiz28@users.noreply.github.com>
2019-12-05 02:15:19 +0600

    Update FileServer.java

--- Networking/FileServer.java
+++ Networking/FileServer.java
@@ -1,55 +1,42 @@
-import java.io.DataInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class FileServer extends Thread {
-	
-	private ServerSocket ss;
-	
-	public FileServer(int port) {
-		try {
-			ss = new ServerSocket(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void run() {
-		while (true) {
-			try {
-				Socket clientSock = ss.accept();
-				saveFile(clientSock);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private void saveFile(Socket clientSock) throws IOException {
-		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
-		FileOutputStream fos = new FileOutputStream("server.txt");
-		byte[] buffer = new byte[4096];
-		
-		int filesize = 15123; 
-		int read = 0;
-		int totalRead = 0;
-		int remaining = filesize;
-		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
-			totalRead += read;
-			remaining -= read;
-			System.out.println("read " + totalRead + " bytes.");
-			fos.write(buffer, 0, read);
-		}
-		
-		fos.close();
-		dis.close();
-	}
+public class FileServer {
 	
 	public static void main(String[] args) {
-		FileServer fs = new FileServer(1988);
-		fs.start();
+		try {
+			ServerSocket server = new ServerSocket(2343);
+			Socket socket = server.accept();
+			DataInputStream din = new DataInputStream(socket.getInputStream());
+			
+			String str;
+			str = (String)din.readUTF();
+			System.out.println("Receaving " + str);
+			
+			FileOutputStream fos = new FileOutputStream(str);
+			byte [] buffer = new byte[4096];
+			
+			int filesize = 1000000000;
+			int read=0;
+			int totalRead = 0;
+			int remaining = filesize;
+			while((read = din.read(buffer,0,Math.min(buffer.length,remaining)))>0){
+				totalRead += read;
+				remaining -= read;
+				System.out.println("read " + totalRead + " bytes.");
+				fos.write(buffer,0,read);
+			}
+			
+			
+			fos.close();
+			din.close();
+			server.close();
+			socket.close();
+			
+		}catch(Exception ex) {
+			System.out.println(ex);
+		}
+
 	}
 
 }
